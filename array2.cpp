f2b4b6244a3847e10c88b6f9481fce9bfb76f5ee
sol1305 <solophinamkandawire@unima.ac.mw>
2024-04-25 09:42:31 -0700

    my first commit

---
+++ array2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+    string array []= new array[8];
+    
+array [0]= 'B123';
+array [1]= 'C234';
+array [2]= 'A354';
+array [3]= 'C15';
+array [4]= 'B177';
+array [5]= 'G3003';
+array [6]= 'C235';
+array [7]= 'B179';
+   
+   for (int a = 0; a < 8; a++);
+    if(array=4 || array=7){
+        cout << array << endl;
+       
+    }
+   else {
+      cout << "the array content does not starts with letter B";
+   }
+return 0;
+}
